eb0922768705ea72e28b69c25b25bb1797012ded
LuDev01 <luisatoro0121@hotmail.com>
2024-10-05 11:18:24 -0500

    Including Mantine library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Animation from './animations/components/Animation'
 import './App.css'
+import '@mantine/core/styles.css';
 
 function App() {
 
   return (
     <>
       <Animation />
+
+
     </>
   )
 }
