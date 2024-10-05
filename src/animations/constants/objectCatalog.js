f2d031df32646bee3fc97d97f0e99a5520c765d1
Daniel Arenas Múnera <danprogramator@gmail.com>
2024-10-05 11:06:23 -0500

    Create objectCatalog.js

---
+++ src/animations/constants/objectCatalog.js
@@ -0,0 +1,52 @@
+export const objectCatalog = {
+    sun: {
+        name: "Sol",
+        magnitude: "-100",
+        tipo: "Estrella",
+      },
+      mercury: {
+        name: "Mercurio",
+        magnitude: "-0.42",
+        tipo: "Planeta",
+      },
+      venus: {
+        name: "Venus",
+        magnitude: "-4.89",
+        tipo: "Planeta",
+      },
+      earth: {
+        name: "Tierra",
+        magnitude: "-3.99",
+        tipo: "Planeta",
+      },
+      mars: {
+        name: "Marte",
+        magnitude: "-1.52",
+        tipo: "Planeta",
+      },
+      jupiter: {
+        name: "Júpiter",
+        magnitude: "-2.94",
+        tipo: "Planeta",
+      },
+      saturn: {
+        name: "Saturno",
+        magnitude: "-0.49",
+        tipo: "Planeta",
+      },
+      uranus: {
+        name: "Urano",
+        magnitude: "5.32",
+        tipo: "Planeta",
+      },
+      neptune: {
+        name: "Neptuno",
+        magnitude: "7.78",
+        tipo: "Planeta",
+      },
+      pluto: {
+        name: "Plutón",
+        magnitude: "13.65",
+        tipo: "Planeta Enano",
+      }
+  };
