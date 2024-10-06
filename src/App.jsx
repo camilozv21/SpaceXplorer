a268583c45c6ef466dab2252d68657607def68fd
LuDev01 <luisatoro0121@hotmail.com>
2024-10-06 12:07:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,25 @@ import Animation from './animations/components/Animation'
 import './App.css'
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
-import { Notifications, showNotification } from '@mantine/notifications';
-import { useEffect } from 'react';
-import { IconComet } from '@tabler/icons-react';
+import { Notifications } from '@mantine/notifications';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing } from './animations/components/Landing';
 
 
 function App() {
-  useEffect(() => {
-    setTimeout(() => {
-      showNotification({
-        title: 'Apophis is nearer than ever!',
-        message: 'Today October 6, 2024 Apophis is at a distance of 68585153.184325793 Km.',
-        color: 'black',
-        icon: <IconComet stroke={2} color='yellow' />,
-        autoClose: 5000
-      });
-    }, 4000);
-  }, []);
+
 
   return (
     <>
+        <MantineProvider>
     <BrowserRouter>
       <Routes>
-        <MantineProvider>
-          <Route path='/' element={<div>hola</div>} />
+          <Route path='/' element={<Landing /> } />
           <Route path='/solar-system' element={<Animation />} />
-          <Notifications position='top-center' withBorder  />
-        </MantineProvider>
       </Routes>
     </BrowserRouter>
+          <Notifications position='top-center'  />
+        </MantineProvider>
     </>
   )
 }
